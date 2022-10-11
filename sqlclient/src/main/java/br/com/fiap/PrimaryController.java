29cdecbfa8d638747d805d589c5ab6aa5f61acc7
Joao <joaocarloslima@me.com>
2022-10-11 11:58:43 -0300

    feat: consulta

--- sqlclient/src/main/java/br/com/fiap/PrimaryController.java
+++ sqlclient/src/main/java/br/com/fiap/PrimaryController.java
@@ -3,12 +3,14 @@ package br.com.fiap;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -35,7 +37,12 @@ public class PrimaryController {
             var comando = con.prepareStatement(sql);
             comando.execute();
 
-            //if (sql.startsWith("SELECT")) carregarDadosNaTabela(resultado);
+            if (sql.toUpperCase().startsWith("SELECT")) {
+                var resultado = comando.executeQuery();
+                carregarDadosNaTabela(resultado);
+            }else{
+                comando.execute();
+            }
 
             status.setText("Comando executado: " + sql);
             historico.getItems().add(sql);
@@ -55,8 +62,24 @@ public class PrimaryController {
         textAreaSql.setText(comando);
     }
 
-    private void carregarDadosNaTabela(ResultSet resultado){
+    private void carregarDadosNaTabela(ResultSet resultado) throws SQLException{
+        var columnCount = resultado.getMetaData().getColumnCount();
+        tabela.getColumns().removeAll(tabela.getColumns());
+        for (int i = 1; i <= columnCount; i++){
+            var columnName = resultado.getMetaData().getColumnLabel(i);
+            var coluna = new TableColumn<ArrayList<String>, String>(columnName);
+            coluna.setCellValueFactory(new CallbackImp(i-1));
+            tabela.getColumns().add(coluna);
+        }
 
+        tabela.getItems().clear();
+        while(resultado.next()){
+            var lista = new ArrayList<String>();
+            for (int i = 1; i <= columnCount; i++){
+                lista.add(resultado.getString(i));
+            }
+            tabela.getItems().add(lista);
+        }
     }
 
 }
